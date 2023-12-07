9b49f8ab7385d7a65b7a1c3ba57282a1156630fa
=domagojleskovic <domagoj.leskovic24@gmail.com>
2023-12-07 16:45:57 +0100

    Add solution for part 2 of day 4

--- Day 4/Solution/Program.cs
+++ Day 4/Solution/Program.cs
@@ -7,20 +7,20 @@ if (!File.Exists(filePath))
     Console.WriteLine("File not found!");
     return;
 }
-int i = 1;
+int y = 1;
 using (StreamReader reader = new StreamReader(filePath))
 {
     string? line;
     while((line = reader.ReadLine()) != null)
     {
         string temp = line.Replace("Card ", "");
-        temp = temp.Replace($"{i}:", "");
+        temp = temp.Replace($"{y}:", "");
         temp = temp.Trim();
         lines.Add(temp);
-        i++;
+        y++;
     }
 }
-
+/*Part 1*//*
 int sum = 0;
 int count;
 foreach(string line in lines)
@@ -41,4 +41,23 @@ foreach(string line in lines)
         sum += (int)Math.Pow(2, count);
     }
 }
-Console.WriteLine($"Sum: {sum}");
+Console.WriteLine($"Sum: {sum}");*/
+int[] cardCount = new int[lines.Count];
+for(int i = 0; i < lines.Count; i++)
+{
+    cardCount[i] = 1;
+}
+
+for(int cardId = 0; cardId < lines.Count; cardId++)
+{
+    String[] row = lines[cardId].Split("|");
+    List<string> winningNumbersStrings = row[0].Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+    List<string> playedNumbersString = row[1].Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+    int matchCount = playedNumbersString.Intersect(winningNumbersStrings).Count();
+    for (int i = 0; i < matchCount; i++)
+    {
+        cardCount[cardId + 1 + i] += cardCount[cardId];
+    }
+}
+Console.WriteLine(cardCount.Sum());
